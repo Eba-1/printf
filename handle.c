2fd7bd4f3b5d8ca3d3d486bb4a51c761c4f7467c
Eba-1 <ebenwoldehan@gmail.com>
2023-04-02 22:55:20 +0530

    New handler file

---
+++ handle.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * handle_print - Prints an argument based on its type
+ * @ls: List of arguments to be printed.
+ * @width: Calculates the width
+ * @size: Specifies the size
+ * @buffer: Buffer array to handle print
+ * @flags:  Checks for flags that are active
+ * @prec: Specifies the precision
+ * @ind: ind
+ * @format: A string format string to print arguments
+ * Return: 1 for true or 2 false
+ */
+int handle_print(const char *fmt, int *ind, va_list ls, char buffer[], int flags, int width, int prec, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(ls, buffer, flags, width, prec, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
