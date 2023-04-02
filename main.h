f01c6ca4acf41d42d505fc9dfc6a2aae7b6371ff
Eba-1 <ebenwoldehan@gmail.com>
2023-04-02 23:22:29 +0530

    Included new functions

--- main.h
+++ main.h
@@ -1,96 +1,81 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <unistd.h>
 
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+#define F_ZERO 4
 #define F_MINUS 1
 #define F_PLUS 2
-#define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
- * struct fmt - Struct op
- *
- * @fmt: The format.
- * @fn: The function associated.
+ * struct form - A structure for operations
+ * @form: The format.
+ * @func: The function associated.
  */
-struct fmt
+struct form
 {
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
+	int (*func)(num_store, char[], int, int, int, int);
+	char form;
 };
 
-
 /**
- * typedef struct fmt fmt_t - Struct op
- *
- * @fmt: The format.
+ * typedef struct form fmt_t - Struct op
  * @fm_t: The function associated.
+ * @form: The format.
  */
-typedef struct fmt fmt_t;
+typedef struct form fmt_t;
 
-int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_int(num_store types, char buffer[], int flags, int width, int prec, int size);
+int print_binary(num_store types, char buffer[], int flags, int width, int prec, int size);
+int print_percent(num_store types, char buffer[], int flags, int width, int prec, int size);
+int print_char(num_store types, char buffer[], int flags, int width, int prec, int size);
+int print_string(num_store types, char buffer[], int flags, int width, int prec, int size);
 
+int print_unsigned(num_store types, char buffer[], int flags, int width, int prec, int size);
+int print_hexa(num_store types, char map_to[], char buffer[], int flags, char flag_ch, int width, int prec, int size);
+int print_octal(num_store types, char buffer[], int flags, int width, int prec, int size);
+int print_hexadecimal(num_store types, char buffer[],
+	int flags, int width, int prec, int size);
+int print_hexa_upper(num_store types, char buffer[], int flags, int width, int prec, int size);
 
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_unsigned(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_octal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
+int flag_num(const char *format, int *r);
 
-int print_hexa(va_list types, char map_to[],
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
-int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-int print_reverse(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_rot13string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-	int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-	int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
+int prec(const char *format, int *arg, num_store ls);
 
-int write_unsgnd(int is_negative, int ind,
-char buffer[],
-	int flags, int width, int precision, int size);
+int size(const char *format, int *arg);
 
+int width(const char *format, int *arg, num_store ls);
+
+int hPrint(const char *form, int *ind, num_store list, char buffer[], int flags, int width, int precision, int size);
+
+int is_digit(char);
 int is_printable(char);
 int append_hexa_code(char, char[], int);
-int is_digit(char);
-
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
+
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
+
+int _printf(const char *format, ...);
+
+int print_non_printable(num_store types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_pointer(num_store types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_reverse(num_store types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_rot13string(num_store types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
